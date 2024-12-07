4203d2a4e028a45a46caf68faf26c26fba6628f5
janregx <kinanprincipes@gmail.com>
2024-12-07 18:49:28 -0400

    Abstracted away html tag creation in [framework.js]

--- framework.js
+++ framework.js
@@ -1,1 +1,23 @@
+function tag(name, ...children) {
+    const result = document.createElement(name);
 
+    for (const child of children) {
+        if (typeof(child) === "string") {
+            result.appendChild(document.createTextNode(children));
+        } else {
+            result.appendChild(child);
+        }
+    }
+
+    result.attribute = function (name, value) {
+        this.setAttribute(name, value);
+        return this;
+    }
+
+    result.clicked = function(callback) {
+        this.onclick = callback;
+        return this;
+    }
+
+    return result;
+}
--- mainpage.js
+++ mainpage.js
@@ -4,3 +4,10 @@ let content = document.getElementById("content");
 
 profile.innerHTML = "<h1>Janregx's Profile</h1>";
 content.innerHTML = "<p>Content of website</p>"
+
+const entry = document.getElementById("entry");
+entry.appendChild(
+    tag("div", 
+        tag("h1", "hello, world!").attribute("style", "color:9999bb; background-color:red")
+    ).attribute("style", "display:flex; align-items:center; justify-content:center")
+);
