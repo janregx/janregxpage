1ed5c032c77920a9879033afb7b44af26364f99b
janregx <kinanprincipes@gmail.com>
2024-12-07 16:59:29 -0400

    initial commit

---
+++ mainpage.js
@@ -0,0 +1,6 @@
+let profile = document.getElementById("profile");
+let content = document.getElementById("content");
+
+
+profile.innerHTML = "<h1>Janregx's Profile</h1>";
+content.innerHTML = "<p>Content of website</p>"
