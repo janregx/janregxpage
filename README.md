4af9e92fe07ea9654c4c1f30bc5588f8b0f414fb
janregx <kinanprincipes@gmail.com>
2024-12-10 17:49:42 -0400

    initial commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# Plans
-
-- [ ] Simple framework to use components inside main page.
-- [ ] Elaborate color palette and structure overall layout.
-- [ ] Deploy
---
+++ src/mainpage.js
@@ -0,0 +1,6 @@
+const entrypoint = document.getElementById("mainpage");
+
+let test = document.createElement("h1")
+
+test = document.createTextNode("Hi there")
+entrypoint.appendChild(test);
