eb94d8bd55dce6d418116d1a7383be44808f31c7
janregx <kinanprincipes@gmail.com>
2024-12-10 21:12:54 -0400

    Stylised window component.

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,6 +1,12 @@
-const entrypoint = document.getElementById("mainpage");
+const win = document.querySelector(".window");
 
-let test = document.createElement("h1")
+// Color palette
+const BG_DARK = "#181C14";
+const BG_MED = "#3C3D37";
+const BG_LIGHT = "#697565";
+const DETAIL = "#ECDFCC";
 
-test = document.createTextNode("Hi there")
-entrypoint.appendChild(test);
+win.style.backgroundColor = BG_MED;
+win.style.border = "2px dashed "+BG_LIGHT;
+
+win.appendChild(document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."));
