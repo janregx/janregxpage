16e2077287d81ad1f78859bc928845bb1a03fda3
Kinan Principe <kinanprincipes@gmail.com>
2024-12-26 17:13:43 -0400

    Laying ground for layout menus and posts

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,9 +1,18 @@
-const panel = document.querySelector(".panel");
+const post1 = document.querySelector(".post1");
+const post2 = document.querySelector(".post2");
+const post3 = document.querySelector(".post3");
+
 
 // Color palette
 
 const IPSEM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
-panel.appendChild(
-    document.createTextNode(IPSEM)
+post1.appendChild(
+    document.createTextNode(IPSEM+IPSEM)
+);
+post2.appendChild(
+    document.createTextNode(IPSEM+IPSEM)
+);
+post3.appendChild(
+    document.createTextNode(IPSEM+IPSEM)
 );
